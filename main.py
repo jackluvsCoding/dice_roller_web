497a5c64676cb831a0f58e4b03fa10f5deb3ac6f
Jack <jackhysell@gmail.com>
2021-10-24 12:13:01 -0500

    Added localhost:8080

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Congratulations, it's a web app!"
+
+
+# These two lines tell Python to start Flask’s development server when the script is executed from the command line.
+# Start Flask's development server and interact with the Python app in browser via: $ 'python3 main.py'
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
